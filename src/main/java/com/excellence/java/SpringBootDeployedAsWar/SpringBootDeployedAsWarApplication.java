8f5023f4ca2c366315c1137c6d8d183ee7ec1325
Indramohan Joshi <indramohanjoshi@gmail.com>
2020-02-09 15:24:23 +0530

    Deploying spring boot microservice application on external web server as war

---
+++ src/main/java/com/excellence/java/SpringBootDeployedAsWar/SpringBootDeployedAsWarApplication.java
@@ -0,0 +1,13 @@
+package com.excellence.java.SpringBootDeployedAsWar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootDeployedAsWarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootDeployedAsWarApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/excellence/java/SpringBootDeployedAsWar/SpringBootDeployedAsWarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.excellence.java.SpringBootDeployedAsWar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDeployedAsWarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
